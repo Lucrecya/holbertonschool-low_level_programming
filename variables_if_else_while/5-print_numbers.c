dd76d092b940add80869e07892c005fe8a8977d2
root <imenfadaoui123@gmail.com>
2023-06-20 03:09:45 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Description: Prints all single digit numbers of base 10 starting from 0,
+*              followed by a new line using the putchar function.
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+{
+putchar(number + '0');
+}
+putchar('\n');
+return (0);
+}
+

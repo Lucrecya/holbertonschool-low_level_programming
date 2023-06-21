d4434304d81a2531ce2f538d78dc9c3779610f40
root <imenfadaoui123@gmail.com>
2023-06-21 13:47:31 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Description: Print all possible combinations of single-digit numbers,
+* separated by comma and space. Numbers are printed in ascending order.
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
+

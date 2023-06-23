4c695ef0add5f454b022ef5745b0df46f94ba847
root <imenfadaoui123@gmail.com>
2023-06-23 13:51:33 -0700

    table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,34 +5,36 @@
  */
 void times_table(void)
 {
-int x, y, z, u, d;
+    int x, y, u, d;
 
-for (x = 0; x <= 9; x++)
-{
-for (y = 0; y <= 9; y++)
-{
-result = x * y;
-if (z > 9)
-{
-u = z % 10;
-d = (z - u) / 10;
-_putchar(44);
-_putchar(32);
-_putchar(d + '0');
-_putchar(u + '0');
+    for (x = 0; x <= 9; x++)
+    {
+        for (y = 0; y <= 9; y++)
+        {
+            int result = x * y;
+
+            if (result > 9)
+            {
+                u = result % 10;
+                d = (result - u) / 10;
+                _putchar(',');
+                _putchar(' ');
+                _putchar(d + '0');
+                _putchar(u + '0');
+            }
+            else
+            {
+                if (y != 0)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                _putchar(result + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
-else
-{
-if (y != 0)
-{
-_putchar(44);
-_putchar(32);
-_putchar(32);
-}
-_putchar(z + '0');
-}
-}
-_putchar('\n');
-}
-}
+
 

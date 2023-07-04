4c1ce5c8dd79dfca44f9ce485e1f75700f6c6201
root <imenfadaoui123@gmail.com>
2023-07-04 09:46:22 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
+	while (!(str[index] >= 'a' && str[index] <= 'z') && !(str[index] >= 'A' && str[index] <= 'Z'))
 			index++;
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
@@ -28,7 +28,11 @@ char *cap_string(char *str)
 		    str[index - 1] == '{' ||
 		    str[index - 1] == '}' ||
 		    index == 0)
-			str[index] = 32;
+		{
+
+		if (str[index] >= 'a' && str[index] <= 'z')
+		    str[index] = str[index] - 32;
+		}
 		index++;
 	}
 
